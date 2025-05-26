7fc8fa3ba4aa7ccf9c11dce00db0ce1e267671c1
Enis Haliti <r0955040@student.thomasmore.be>
2025-05-26 17:36:36 +0200

    another commit

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,24 @@
+// import { defineConfig } from "cypress";
+
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
+
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "hg8yo2", // ← your project ID from Cypress Cloud
+
   e2e: {
+    baseUrl: "https://r0955040-realbeans.myshopify.com", // ← your Shopify dev store
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
